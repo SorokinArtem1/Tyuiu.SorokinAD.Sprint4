a31de461d3f886a14ee86a410e868493e89aa4a8
Tyuiu.SorokinAD@outlook.com <abuzius@mail.ru>
2023-11-05 15:41:38 +0500

    Task3

---
+++ Tyuiu.SorokinAD.Sprint4.Task3.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SorokinAD.Sprint4.Task3.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SorokinAD.Sprint4.Task3.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc3da5c2-9c1d-4d57-91ef-cf4c248fb653")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SorokinAD.Sprint4.Task3.V22/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SorokinAD.Sprint4.Task3.V22.Lib;
+
+namespace Tyuiu.SorokinAD.Sprint4.Task3.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int[,] array = new int[5, 5] { { 4, 4, 7, 8, 9 }, 
+                                           { 9, 5, 9, 7, 8 }, 
+                                           { 7, 4, 9, 4, 6 }, 
+                                           { 4, 4, 7, 4, 4 }, 
+                                           { 4, 5, 8, 6, 7 } };
+            
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            Console.Title = "Спринт #4| Выполнил: Сорокин А. Д. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил Сорокин А. Д. | ИИПб-23-2                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму нечетных *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Резльтат                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Максимальный элемент в первой строке массива = " + ds.Calculate(array));
+            Console.ReadKey();
+        }
+    }
+}
